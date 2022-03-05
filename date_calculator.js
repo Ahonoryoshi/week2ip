9da7dc32b1e4da50a81b83e9b6e1e6e7558924fe
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-06 02:26:22 +0300

    add warning message for invalid year number input

--- date_calculator.js
+++ date_calculator.js
@@ -4,5 +4,9 @@ while(c<16 || c>99){
     c=parseInt(prompt("Enter the 'FIRST TWO DIGITS' of the year you were born"));
 }
 let y=parseInt(prompt("Enter the 'LAST TWO DIGITS' of the year youy were born"));
+while(y<0 || y>99){
+    alert("Please enter a valid year. MUST be an integer, at least 0 and at most 99");
+    y=parseInt(prompt("Enter the 'LAST TWO DIGITS' of the year youy were born"));
+}
 let m=parseInt(prompt("Enter the 'MONTH' number you were born"));
 let d=parseInt(prompt("Enter the 'DAY' of the month you were born"));
