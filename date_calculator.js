f726c357bf1588f8e851b669bf959d14eac9b918
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-06 02:17:49 +0300

    assign variable d to prompt user input of their birth day of the month

--- date_calculator.js
+++ date_calculator.js
@@ -1,3 +1,4 @@
 let c=parseInt(prompt("Enter the 'FIRST TWO DIGITS' of the year you were born"));
 let y=parseInt(prompt("Enter the 'LAST TWO DIGITS' of the year youy were born"));
 let m=parseInt(prompt("Enter the 'MONTH' number you were born"));
+let d=parseInt(prompt("Enter the 'DAY' of the month you were born"));
