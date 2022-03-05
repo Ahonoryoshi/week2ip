35f5889dbf30a1a0dfba7846952d98b61501947b
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-06 02:22:58 +0300

    add a warning message for invalid century number input

--- date_calculator.js
+++ date_calculator.js
@@ -1,4 +1,8 @@
 let c=parseInt(prompt("Enter the 'FIRST TWO DIGITS' of the year you were born"));
+while(c<16 || c>99){
+    alert("Please enter a valid number. MUST be an integer, at least 16 and at most 99");
+    c=parseInt(prompt("Enter the 'FIRST TWO DIGITS' of the year you were born"));
+}
 let y=parseInt(prompt("Enter the 'LAST TWO DIGITS' of the year youy were born"));
 let m=parseInt(prompt("Enter the 'MONTH' number you were born"));
 let d=parseInt(prompt("Enter the 'DAY' of the month you were born"));
