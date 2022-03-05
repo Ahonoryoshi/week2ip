0636ba0727080d0be898d178bc7ddfbce2d2bf0d
Ahonoryoshi <yoshiahona@gmail.com>
2022-03-06 02:32:02 +0300

    add warning message for invalid day input

--- date_calculator.js
+++ date_calculator.js
@@ -9,4 +9,11 @@ while(y<0 || y>99){
     y=parseInt(prompt("Enter the 'LAST TWO DIGITS' of the year youy were born"));
 }
 let m=parseInt(prompt("Enter the 'MONTH' number you were born"));
+while(m<1 || m>12){
+    alert("Please enter a valid month. MUST be an integer, at least 1 and at most 12");
+    m=parseInt(prompt("Enter the 'MONTH' number you were born"));
+}
 let d=parseInt(prompt("Enter the 'DAY' of the month you were born"));
+while(d<1 || d>31){
+    alert("Please enter a valid day. MUST be an integer, at least 1 and at most 31")
+}
